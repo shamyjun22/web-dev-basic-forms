1ce4f4944bbc760f02051f8b2fa91f60c94f041b
shamyjun22 <shamyjun22@gmail.com>
2019-09-08 15:38:41 +0800

    Get input

---
+++ main.js
@@ -0,0 +1,6 @@
+function getInput() {
+    let input = document.getElementById("text-input").value;
+    let ouput = document.querySelector("#text-output");
+    output.setAttribute("value", output);
+}
+
